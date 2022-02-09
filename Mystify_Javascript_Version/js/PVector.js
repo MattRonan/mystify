34bc8f7e894d3b1e8c4a5451762383e1c8a0438e
Matt Ronan <mattRonanDev@gmail.com>
2022-02-09 14:33:15 -0500

    Update PVector.js

--- Mystify_Javascript_Version/js/PVector.js
+++ Mystify_Javascript_Version/js/PVector.js
@@ -1,66 +1,66 @@
 //----------------------------constructor
 function PVector(x,y){
-	this.x = x;
-	this.y = y;
-	this.magUp = 0;//TODO should track mag and only recalculate when neccessary.  
+  this.x = x;
+  this.y = y;
+  this.magUp = 0;//TODO should track mag and only recalculate when neccessary.  
 }
 
 //----------------------------static methods
 PVector.add = function(v1,v2){
-	var v3 = new PVector(v1.x+v2.x,v1.y+v2.y);
-	return v3;
+  var v3 = new PVector(v1.x+v2.x,v1.y+v2.y);
+  return v3;
 }
 
 //as in Java, v2 is subtracted from v1
 PVector.sub = function(v1,v2){
-	var v3 = new PVector(v1.x-v2.x,v1.y-v2.y);
-	return v3;
+  var v3 = new PVector(v1.x-v2.x,v1.y-v2.y);
+  return v3;
 }
 
 //------------------------------instance methods
 PVector.prototype.get = function(v){
-	var v3 = new PVector(v.x,v.y);
-	return v3;
+  var v3 = new PVector(v.x,v.y);
+  return v3;
 }
 
 PVector.prototype.add = function(v){
-	this.x += v.x;
-	this.y += v.y;
-	this.magUp = 1;
+  this.x += v.x;
+  this.y += v.y;
+  this.magUp = 1;
 }
 
 PVector.prototype.sub = function(v){
-	this.x -= v.x;
-	this.y -= v.y;
+  this.x -= v.x;
+  this.y -= v.y;
 }
 
 PVector.prototype.mult = function(v){
-	this.x *= v.x;
-	this.y *= v.y;
+  this.x *= v.x;
+  this.y *= v.y;
 }
 
 PVector.prototype.div = function(v){
-	this.x /= v.x;
-	this.y /= v.y;
+  this.x /= v.x;
+  this.y /= v.y;
 }
 
 PVector.prototype.mag = function(){
-	var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
-	return magnitude;
+  var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
+  return magnitude;
 }
 
 PVector.prototype.normalize = function(v){
-	var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
-	this.x = this.x/magnitude;
-	this.y = this.y/magnitude;
+  var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
+  this.x = this.x/magnitude;
+  this.y = this.y/magnitude;
 }
 
 PVector.prototype.limit = function(lim){
-	var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
-	
-	if(magnitude > lim){
-		var fact = magnitude / lim;
-		this.x = this.x/fact;
-		this.y = this.y/fact;
-	}
+  var magnitude = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) );
+  
+  if(magnitude > lim){
+    var fact = magnitude / lim;
+    this.x = this.x/fact;
+    this.y = this.y/fact;
+  }
 }
